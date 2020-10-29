a8cbc38f70807e311120eef2aaa8347848e848ca
nanakuma <nanakumananakuma13096@gmail.com>
2020-10-29 12:43:06 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  #validates 門番みたいなもの 無効なデータでないことを検証する
+  #presence 空のデータを登録できないようにする
 end
